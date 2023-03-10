dda7db6e2aa08d47d47e6d6a05bc843ec762ed04
Andre Sharpe <andre.sharpe@gmail.com>
2023-03-10 15:31:49 +0100

    Refactored PhoneNumberExtensions

--- src/Nox.Utilities.PhoneNumberInfo/PhoneNumberExtensions.cs
+++ src/Nox.Utilities.PhoneNumberInfo/PhoneNumberExtensions.cs
@@ -11,58 +11,66 @@ public static partial class PhoneNumberExtensions
 
     public static PhoneNumberInfo GetPhoneInfo(this string inputNumber, string? region = null)
     {
-        var info = new PhoneNumberInfo();
-
         var phoneNumber = _phoneUtil.Parse(inputNumber, region);
 
-        info.InputNumber = inputNumber;
+        var phoneInfo = new PhoneNumberInfo();
 
-        info.IsValid = _phoneUtil.IsValidNumber(phoneNumber);
+        phoneInfo.InputNumber = inputNumber;
+
+        phoneInfo.IsValid = _phoneUtil.IsValidNumber(phoneNumber);
         
-        if (info.IsValid)
+        if (phoneInfo.IsValid)
         {
-            info.FormattedNumber = _phoneUtil.Format(phoneNumber, PhoneNumberFormat.E164);
-            info.FormattedNumberNational = _phoneUtil.Format(phoneNumber, PhoneNumberFormat.NATIONAL);
-            info.FormattedNumberInternational = _phoneUtil.Format(phoneNumber, PhoneNumberFormat.INTERNATIONAL);
-            info.FormattedNumberRfc3966 = _phoneUtil.Format(phoneNumber, PhoneNumberFormat.RFC3966);
-            info.RegionCode = _phoneUtil.GetRegionCodeForNumber(phoneNumber);
-            info.NumberType = _phoneUtil.GetNumberType(phoneNumber).ToString() ?? "";
-            info.IsMobile = !string.IsNullOrEmpty(info.NumberType) && info.NumberType.Equals("MOBILE");
-            info.RegionName= _geoCoder.GetDescriptionForNumber(phoneNumber, Locale.English);
-            info.CarrierName = info.FormattedNumber.GetCarrier();
+            phoneInfo.FormattedNumber = _phoneUtil.Format(phoneNumber, PhoneNumberFormat.E164);
+
+            phoneInfo.FormattedNumberNational = _phoneUtil.Format(phoneNumber, PhoneNumberFormat.NATIONAL);
+
+            phoneInfo.FormattedNumberInternational = _phoneUtil.Format(phoneNumber, PhoneNumberFormat.INTERNATIONAL);
+
+            phoneInfo.FormattedNumberRfc3966 = _phoneUtil.Format(phoneNumber, PhoneNumberFormat.RFC3966);
+
+            phoneInfo.RegionCode = _phoneUtil.GetRegionCodeForNumber(phoneNumber);
+
+            phoneInfo.NumberType = _phoneUtil.GetNumberType(phoneNumber).ToString() ?? "";
+
+            phoneInfo.IsMobile = !string.IsNullOrEmpty(phoneInfo.NumberType) && phoneInfo.NumberType.Equals("MOBILE");
+
+            phoneInfo.RegionName= _geoCoder.GetDescriptionForNumber(phoneNumber, Locale.English);
+
+            phoneInfo.CarrierName = phoneInfo.FormattedNumber.GuessCarrier();
         }
-        return info;
+        return phoneInfo;
     }
 
-        public static string GetCarrier(this string fullNumber)
-        {
-            var key = Convert.ToInt32(fullNumber.TrimStart('+').PadRight(9,'0')[..9]);
+    private static string GuessCarrier(this string formattedNumber)
+    {
+        var key = Convert.ToInt32(formattedNumber.TrimStart('+').PadRight(9,'0')[..9]);
 
-            int min = 0;
-            int max = _carrierMap.Length - 1;
-            int mid;
-            while (min <= max)
+        int min = 0;
+        int max = _carrierMap.Length - 1;
+        int mid;
+        while (min <= max)
+        {
+            mid = (min + max) / 2;
+            if (key == _carrierMap[mid,0])
             {
-                mid = (min + max) / 2;
-                if (key == _carrierMap[mid,0])
-                {
-                    max = mid;
-                    break;
-                }
-                else if (key < _carrierMap[mid,0])
-                {
-                    max = mid - 1;
-                }
-                else
-                {
-                    min = mid + 1;
-                }
+                max = mid;
+                break;
             }
-
-            if (min < 1 || max > _carrierMap.Length-1) return "Unknown";
-     
-            return _carriers[_carrierMap[max, 1]];
-
+            else if (key < _carrierMap[mid,0])
+            {
+                max = mid - 1;
+            }
+            else
+            {
+                min = mid + 1;
+            }
         }
+
+        if (min < 1 || max > _carrierMap.Length-1) return "Unknown";
+     
+        return _carriers[_carrierMap[max, 1]];
+
     }
+}
 
